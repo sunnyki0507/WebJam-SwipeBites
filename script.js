afa575951b934218ffbdd1c8e1c5aa156cbee461
to-claire <clockwork.cto@gmail.com>
2023-11-15 17:44:03 -0800

    hmmm

--- script.js
+++ script.js
@@ -6,40 +6,82 @@ const options = {
     }
 };
 
-function get_details(id) {
-    fetch(`https://api.yelp.com/v3/businesses/${encodeURIComponent(id)}`, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-}
-
 function get_business(location, price) {
     const url = `https://api.yelp.com/v3/businesses/search?location=${encodeURIComponent(location)}&price=${encodeURIComponent(price)}`;
 
-    fetch(url, options)
+    return fetch(url, options)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => response)
         .catch(err => console.error(err));
 }
 
-function returnValues() {
+function get_details(id) {
+    return fetch(`https://api.yelp.com/v3/businesses/${encodeURIComponent(id)}`, options)
+        .then(response => response.json())
+        .then(response => response)
+        .catch(err => console.error(err));
+}
+
+function return_values() {
     let locationInput = document.getElementById("locationInput").value;
-    alert(locationInput);
 
     let price = [];
     let count = "$";
-    for(let x = 0; x < 4; ++x)
-    {
+    for (let x = 0; x < 4; ++x) {
         if (document.getElementById(count).checked) {
             price.push(x + 1);
         }
         count += "$";
     }
 
-    alert(price);
+    return {
+        location: locationInput,
+        price: price
+    };
 }
-function radioValue(clickedButton){
+
+function radioValue(clickedButton) {
     let itemID = clickedButton.id;
     let radio = document.getElementById(itemID);
     radio.value = "true";
 }
+
+async function retrieve_data(attribute) {
+    try {
+        // const input = return_values();
+        const location = "irvine";
+        const price = [1, 2];
+
+        const businessData = await get_business(location, price);
+
+        if (businessData.businesses.length > 0) {
+            const randomIndex = Math.floor(Math.random() * businessData.businesses.length);
+            const detailsData = await get_details(businessData.businesses[randomIndex].id);
+
+            if (detailsData) {
+                let value = detailsData[attribute];
+                return value;
+            } else {
+                console.error('Details not found.');
+                return null;
+            }
+        } else {
+            console.error('No businesses found.');
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+async function get(attribute) {
+    try {
+        let output = await retrieve_data(attribute);
+        console.log(output);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+get("name");
